ad3586d3a72640067de1d3a69fca791ad89215b1
Ana Elles <alelles16@gmail.com>
2018-06-20 08:38:48 -0500

    Update Models

--- foodManager/admin.py
+++ foodManager/admin.py
@@ -1,1 +1,5 @@
 from django.contrib import admin
+from .models import Producto, Registro
+
+admin.site.register(Producto)
+admin.site.register(Registro)
--- foodManager/models.py
+++ foodManager/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+from django.utils import timezone
+
+class Producto (models.Model):
+    nombre = models.CharField(max_length=200)
+    marca = models.CharField(max_length=200)
+    cantidad_actual = models.PositiveIntegerField(default=0)
+
+    def __str__ (self):
+        return self.nombre
+
+class Registro (models.Model):
+    producto = models.ForeignKey('Producto', on_delete=models.CASCADE)
+    usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    fecha = models.DateTimeField(blank=True, null=True, editable=False)
+    cantidad_comprada = models.PositiveIntegerField(default=0)
+
+    def nuevo_registro (self):
+        self.fecha = timezone.now()
+        self.producto.cantidad_actual -= self.cantidad_comprada
+        self.producto.save()
+    
+    def save (self, *args, **kwargs):
+        self.nuevo_registro()
+        super(Registro, self).save(*args, **kwargs)
+
+    def __str__ (self):
+        return '{} - {}'.format(self.producto.nombre, self.cantidad_comprada)
