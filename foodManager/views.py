01eca60feb577308ff3dae68c29f20c7f6fbb282
Ana Elles <alelles16@gmail.com>
2018-06-20 09:46:41 -0500

    Add html files

--- foodManager/views.py
+++ foodManager/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Producto
+
+def producto_list(request):
+    productos = Producto.objects.order_by('cantidad_actual')
+    return render(request, 'foodManager/producto_list.html',{'productos' : productos})
